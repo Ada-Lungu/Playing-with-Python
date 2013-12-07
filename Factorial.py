56badfb54a1fbe747a486e960b507a85e6b5582b
Ada Lungu <ada.lungu@gmail.com>
2013-12-07 16:11:16 +0100

    first commit

---
+++ Factorial.py
@@ -0,0 +1,16 @@
+
+ def factorial(n):  # n! = n(n-1)! ; 0! = 1
+ 	if n == 0:
+ 		return 1     	# factorial = 1 ????
+ 	else:
+ 		factorial = n * factorial(n-1)  # or return  n * factorial(n-1)
+ 		return factorial 
+ 	
+# this is a Recursive Function = calling a function inside itself
+
+def fibonacci(n): # fibonacci(n) = fibonacci (n-1) + fibonacci (n-2)
+	if n == 0 or  n == 1:
+		fibonnaci = 1
+	else:
+		fibonnaci = fibonnaci(n-1) + fibonnaci(n-2)
+		return fibonnaci
