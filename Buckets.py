fe2a2c29ec43d6c48d9f3a3dadbc312830aa63ae
Mircea Lungu <mircea.lungu@gmail.com>
2013-12-07 16:53:50 +0100

    added the random_list_with_ints function

--- Buckets.py
+++ Buckets.py
@@ -1,9 +1,20 @@
 import random
+import math
+
+def random_list_with_ints(num):
+	lst = random_list(num)
+	new_list = [0] * num
+	for i in range(num):
+		new_list [i] = math.floor(lst[i] * 100)
+	return new_list	
+		
 
 def random_list(num):
 	my_list = [0] * num
 	for i in range(num):
 		my_list[i] = random.random()
 	return my_list
+
+print random_list_with_ints(8)
 
 
