1bfbecf83a9987eddda5d86f07bb5c768664cb1e
Ada Lungu <ada.lungu@gmail.com>
2013-12-07 17:31:04 +0100

    Counting elements in buckets

--- Buckets.py
+++ Buckets.py
@@ -1,9 +1,47 @@
 import random
 
+# imi da o lista cu num numere random
 def random_list(num):
 	my_list = [0] * num
 	for i in range(num):
 		my_list[i] = random.random()
 	return my_list
 
+# doresc sa impart lista in mai multe buckets num_buckets, lista mea are valori intre  0.0 - 1.0
+#==> width-ul fiecarei liste in aflu 1.0/num_buck
+
+# calculez cate elemente sunt in fiecare bucket
+
+
+def count_elem_in_buck(my_list, low, high):
+	count = 0
+	for num in my_list:
+		if  low < num< high:
+			count += 1
+	return count
+
+
+def print_range_buck(num_buckets):
+
+	width_bucket = 1.0/num_buckets
+
+	for i in range(num_buckets):
+		low = i * width_bucket
+		high = low + width_bucket
+		print "range from", low, "to", high
+
+def count_elem_in_buckets(my_list, num_buckets):
+
+	width_bucket = 1.0/num_buckets
+	elem_each_buck = [0] * num_buckets
+
+	for i in range(num_buckets):
+		low = i * width_bucket
+		high = low + width_bucket
+		elem_each_buck[i] = count_elem_in_buck( my_list, low, high)
+	return elem_each_buck
+
+
+print_range_buck(6) 
+print count_elem_in_buckets(random_list(100), 6 )
 
