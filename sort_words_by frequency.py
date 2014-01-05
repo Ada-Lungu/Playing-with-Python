b36c024bcd479f8c7f24d48a981f374f56159438
Ada Lungu <ada.lungu@gmail.com>
2014-01-05 14:09:35 +0200

    latest review

--- sort_words_by frequency.py
+++ sort_words_by frequency.py
@@ -30,13 +30,73 @@ def frequency_categorize(frequent_words):
             elif 500 < frequent_words.index(each_word) < 1000:
                 entre_500_1000.append(each_word)
                 top_frequency["500-1000"] = str(entre_500_1000)
-            else:
+            elif frequent_words.index(each_word) > 1000:
                 over_1000.append(each_word)
                 top_frequency["over 1000"] = str(over_1000)
-        else:
-            print "No frequency for this one."
+            else:
+                print "No frequency for this one."
     return top_frequency
 
 
 frequent_words = ['de', 'la', 'et', 'les', 'des', 'en', 'un', 'du', 'dans', 'pour']
 print frequency_categorize(frequent_words)
+
+
+# gives the position of a word
+
+top1000frtxt=open("top10000fr.txt", "r")
+all_words = top1000frtxt.readlines() #slow
+
+def word_position(the_word):
+    for each_word in all_words:
+        just_the_word = each_word[:-1]
+        if just_the_word == the_word:
+            return all_words.index(each_word)
+
+
+import time
+
+start = time.clock()
+for i in range(1000):
+    word_position('variant')
+end = time.clock()
+print "method 1: %.2gs" % (end-start)
+
+
+def word_position_line_by_line(the_word, file_name):
+    freq_file = open(file_name, "r")
+    line_nr = -1
+    for line in freq_file:
+        line_nr += 1
+        just_the_word = line[:-1]
+        if just_the_word == the_word:
+            return line_nr
+    freq_file.close()
+    return -1
+
+start = time.clock()
+for i in range(1000):
+    word_position_line_by_line('variant', 'top10000fr.txt')
+end = time.clock()
+print "method 2: %.2gs" % (end-start)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ word_importance_level.py
@@ -0,0 +1,62 @@
+
+top1000frtxt=open("top10000fr.txt", "r")
+all_words = top1000frtxt.readlines() #slow - lista cuvintelor frecventa cu /n
+all_words_without_space = []
+for each_word in all_words:
+    each_word_without_space = each_word[:-1]
+    all_words_without_space.append(each_word_without_space)
+
+
+def importance_range(the_word, frequency_list):
+    position = frequency_list.index(the_word)
+    return (position // 1500) + 1
+
+print importance_range("jambes", all_words_without_space)
+
+
+# alte variante de rezolvare
+
+def importance_range3(the_word, frequency_list):
+    position = frequency_list.index(the_word)
+    if position < 1500:
+        return 1
+    elif 1500 < position < 3000:
+        return 2
+    elif 3000 < position < 4500:
+        return 3
+    elif 4500 < position < 6000:
+        return 4
+    elif 4500 < position < 6000:
+        return 5
+    elif 6000 < position < 7500:
+        return 6
+    elif position > 7500:
+        return 7
+
+print importance_range3("jambes", all_words_without_space)
+
+
+def importance_range2(the_word, frequency_list):
+    position = frequency_list.index(the_word)
+
+    return (position // 1500) + 1
+
+    if position // 1500 == 0:
+        return 1
+    elif position // 1500 == 1:
+        return 2
+    elif position // 1500 == 2:
+        return 3
+    elif position // 1500 == 3:
+        return 4
+    elif position // 1500 == 4:
+        return 5
+    elif position // 1500 == 5:
+        return 6
+    else:
+        return 7
+
+
+print importance_range2("jambes", all_words_without_space)
+
+
