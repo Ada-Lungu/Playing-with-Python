ed0eacbbbb2b139bbe2ae854daedee956d84629c
Ada Lungu <ada.lungu@gmail.com>
2014-01-14 11:22:38 +0200

    Revised, works. Binary search Half-works

--- WordFrequencyList_polished.py
+++ WordFrequencyList_polished.py
@@ -1,17 +1,8 @@
+# gives a list of the words and their frequencies
 
-# this gives a dictionary with the words and their frequencies freq_dict = {du:1, aux:2, au:3, le:4}
-def dict_of_word_frequency(freq_list):
-    freq_dict = {}
-    for i in range(1,len(freq_list)): #0,1,2,3 .. 10 000
-        for each_word in freq_list:
-            freq_dict[each_word] = i
-    return freq_list
-
-# gives a list of the words and their frequencies - easier to work with - FINAL VERSION
 def list_of_word_frequency2(freq_list):
     final_list = []
     position_word = 0
-    # for i in range(1,len(freq_list)):
     for each_word in freq_list:
             position_word +=1
             new_list = []
@@ -21,6 +12,7 @@ def list_of_word_frequency2(freq_list):
     return final_list
 
 # [[du,1], [le,2], [aux,3]] gives the frequency of a word given
+# it works just for some of the words in the list, the second part of the list - ?
 def binary_search(freq_list, the_word):
     left = 0
     right = len(freq_list)-1
@@ -51,4 +43,3 @@ clear_list = ["de", "du", "aux", "bi", "retro", "fix"]
 
 print list_of_word_frequency2(clear_list)
 print binary_search(list_of_word_frequency2(clear_list), "de")
-
