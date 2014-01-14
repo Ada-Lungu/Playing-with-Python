66c48f23ff12e786141078f60a744d622d130602
Ada Lungu <ada.lungu@gmail.com>
2014-01-14 11:12:02 +0200

    simplified:) = scraching the right ear with the right hand:)

--- BubblesortAlgorithm.py
+++ BubblesortAlgorithm.py
@@ -31,13 +31,14 @@ def bubble_sort3(my_list):  #[2, 3, 5, 1, 8]
     for i in range(0,len(my_list)):
         for j in range(i+1, len(my_list)):
             if my_list[i] > my_list[j]:
-                a, b = my_list.index(my_list[i]), my_list.index(my_list[j]) # indexul elementelor mele de comparat
-                my_list[a], my_list[b] = my_list[b], my_list[a]
+                """a, b = my_list.index(my_list[i]), my_list.index(my_list[j]) # indexul elementelor mele de comparat
+                my_list[a], my_list[b] = my_list[b], my_list[a]"""
+                my_list[i], my_list[j] = my_list[j], my_list[i]
         count +=1
     print count
     return my_list
 
-lista_mea = [2, 3, 5, 1, 8, 11, 18, 24, 20, 19, 40, 38, 21,50,34,59,52,56,42,90,82,85,83,98]
+lista_mea = [2, 3, 5, 1, 8, 11, 18, 24, 20, 19, 40, 38, 21,50,34,59,52,56,42,90,82,85,83,98, 76, 72, 79, 63, 34]
 print bubble_sort3(lista_mea)
 
 
