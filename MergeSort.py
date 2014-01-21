6ca293a0cbcb909d6699f9a3cc92d78e1c557801
Mircea Lungu <mircea.lungu@gmail.com>
2014-01-21 16:38:27 +0100

    small fixes to merge sort

--- MergeSort.py
+++ MergeSort.py
@@ -1,38 +1,53 @@
-
+# takes two sorted lists
+# must return a new sorted list
 def merge_arrays(left_array, right_array):
     i = 0
     j = 0
-    k = 0
     my_array = []
-    while i < len(left_array) and j < len(right_array):
-        if left_array[i] < right_array[j]:   # lista[index] = elementul (de pe index)
-            my_array.append(left_array[i])
-            i +=1
-        if left_array[i] > right_array[j]:
+    while i < len(left_array) or j < len(right_array):
+
+        # we are finished with left array
+        if (i == len(left_array)):
+            # copy everything that remains from the right_array
             my_array.append(right_array[j])
-            j +=1
+            j+=1
+        elif (j == len(right_array)):
+            #copy everything that remains from left array
+            my_array.append(left_array[i])
+            i+=1
+        elif (left_array[i] <= right_array[j]):   # lista[index] = elementul (de pe index)
+            my_array.append(left_array[i])
+            i += 1
+        elif left_array[i] > right_array[j]:
+            my_array.append(right_array[j])
+            j += 1
     return my_array
 
+# left_array = [2, 5, 7, 9]
+# right_array = [1, 3, 8, 11]
+# print merge_arrays(left_array, right_array)
 
-def split_array(my_array):
-    middle_array = len(my_array)//2 # the index of the mid elem
+# always returns an array which is sorted...
+def split_and_merge_array(my_array):
+    middle_array = len(my_array) // 2 # the index of the mid elem
     left_array = []
     right_array = []
 
-    if len(my_array) < 2: # eu am facut initial cu while len(my_array) >1
-        return
+    if len(my_array) < 2: # an array with one element, is ... sorted!
+        return my_array
     else:
-        for i in range(0,middle_array):
-                left_array.append(my_array[i]) # creeaza stanga
-        for i in range(middle_array+1, len(my_array)-1):
-                right_array.append(my_array[i]) # creeaza dreapta
+        #split
+        for i in range(0, middle_array):
+            left_array.append(my_array[i]) # creeaza stanga
+        for i in range(middle_array, len(my_array)):
+            right_array.append(my_array[i]) # creeaza dreapta
 
-    split_array(left_array)
-    split_array(right_array)
-    return merge_arrays(left_array,right_array, my_array)
+    left_sorted_array = split_and_merge_array(left_array)
+    right_sorted_array = split_and_merge_array(right_array)
+    #merge...
+    return merge_arrays(left_sorted_array, right_sorted_array)
 
+print split_and_merge_array([10,20,4,1,4,2,8])
 
-left_array = [2,5,7,9]
-right_array = [4,3,1,8]
-
-print merge_arrays(left_array, right_array)
+print split_and_merge_array([])
+print split_and_merge_array([-1])
