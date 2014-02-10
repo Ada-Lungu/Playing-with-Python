2ac6d52a1020b46717b2e560cc87ef8bd025629b
Ada Lungu <ada.lungu@gmail.com>
2014-02-10 10:46:56 +0200

    DRYer:)

--- shorten_paragraph.py
+++ shorten_paragraph.py
@@ -4,35 +4,29 @@
 # 3. textul > 42 cuvinte & nu include cuv in primele 42 ==> pana la cuvant
 
 
-def extract_shorter_relevant_example(text, my_word):
+def shorten_word_context(text, given_word, max_length):
     final_text = ""
     forty_list=[]
-    list_words = text.split() # ==> gives me a list of the words ["these", "types", ",", "the"]
-    nr_words = len(list_words)
-    index_my_word = list_words.index(my_word)
-    string_list_words = ' '.join(list_words)
+    list_of_text = text.split() # ==> gives me a list of the words ["these", "types", ",", "the"]
+    word_count = len(list_of_text)
 
-    if nr_words <= 42:
-        final_text = final_text + string_list_words
-        list_final_text = final_text.split()
-        return nr_words, len(list_final_text), final_text
-    #  len(list_words)=10
-    for i in range(0,43):
-        forty_list.append(list_words[i]) # lista cu primele 42 cuvinte
+    if word_count <= max_length:
+        return text.capitalize()
+    #  len(list_of_text)=10
+    for i in range(0,max_length):
+        forty_list.append(list_of_text[i]) # lista cu primele 42 cuvinte
     string_forty_list = ' '.join(forty_list) # string cu primele 42 cuv
 
-    if index_my_word <= 42:
+    if list_of_text.index(given_word) <= max_length:
         final_text = final_text + string_forty_list
-        list_final_text = final_text.split()
-        return nr_words, len(list_final_text), final_text
+        return final_text.capitalize()
 
     else:
-        for i in range(43, index_my_word+1):
-            forty_list.append(list_words[i])
+        for i in range(max_length + 1,  list_of_text.index(given_word) + 1):
+            forty_list.append(list_of_text[i])
             string_forty_list = ' '.join(forty_list)
         final_text = final_text + string_forty_list
-        list_final_text = final_text.split()
-        return nr_words, len(list_final_text), final_text
+        return final_text.capitalize()
 
 text_1 = "once one is at level b1 he is already brave enough to start reading texts in the target language. however, he will still encounter word that he does not understand quite often. thus, he theoretically could start reading literature on the target language, but practically, it is so annoying to constantly search for words will be so boring as to completely repel him. "
 word_1 = "encounter"
@@ -43,9 +37,11 @@ word_3 = "boring"
 text_2 = "we are together all in this one, no matter what they say;)"
 word_2 = "what"
 
-print extract_shorter_relevant_example(text_1, word_1)
-print extract_shorter_relevant_example(text_3, word_3)
-print extract_shorter_relevant_example(text_2, word_2)
+max_lenght = 42
+
+print shorten_word_context(text_1, word_1, max_lenght)
+print shorten_word_context(text_3, word_3, max_lenght)
+print shorten_word_context(text_2, word_2, max_lenght)
 
 mircea_text = ""
 for i in range(0,60):
