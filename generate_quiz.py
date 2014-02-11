a6c1f1d593383eabb336034387d5085727b27920
Mircea Lungu <mircea.lungu@gmail.com>
2014-02-11 12:18:00 +0100

    a few more observations

--- generate_quiz.py
+++ generate_quiz.py
@@ -3,7 +3,7 @@ def generate_quiz(text, word):
 
     list_text_up_to_word = []
     list_text_after_word = []
-    list_of_text = text.split() #["","","",""] ['ce', 'sa', 'mai', 'povestim', 'ca', 'este', 'fara', 'sens.']
+    list_of_text = text.split() 
     missing_word = ""
 
     for i in range(0, list_of_text.index(word)):
@@ -15,7 +15,9 @@ def generate_quiz(text, word):
     #  sa faci join? sau este suficient sa faci join dupa ce ai 
     #  lista calculata deja? 
 
-    for j in range(list_of_text.index(word)+1, len(list_of_text)):
+    # ML: capatul din dreapta al range-ului imi pare putzin prea la dreapta!!
+    #  tzie?
+    for j in range(list_of_text.index(word)+1, len(list_of_text)+1):
         list_text_after_word.append(list_of_text[j])
         text_after_word = ''.join(list_text_after_word)
     # ML: Same as before
@@ -27,8 +29,13 @@ def generate_quiz(text, word):
     # ML: pe langa asta, functzia trebuie sa returneze doar textul. asa am cerut... :)
     return list_of_text, text_up_to_word + ' ' + missing_word + text_after_word
 
+# ML: Textul acesta imi pare mai interesant decat cel vechi :)
 text1 = "Mihai Eminescu este al 7-lea dintre cei 11 copii ai caminarului Gheorghe Eminovici."
+# ML: de ce se numeste variabila precedenta text1? 
 word1 = "este"
+# ML: Si de ce se numeshte asta word1? Sper ca nu te temi
+# cumva ca ar conflict-ui cu cele din definitzia functziei!! 
+# acelea sunt in mod evident vizibile doar in interiorul functziei :)
 
 print generate_quiz(text1, word1)
 
