1ba51c7f3f6c7891cc075dacfd0166f5f3ec75e5
Ada Lungu <ada.lungu@gmail.com>
2014-03-27 15:48:20 +0200

    2 variante - both functioning

--- PYChallenge2.py
+++ PYChallenge2.py
@@ -14,8 +14,11 @@ def decryption(crypted_text, the_alphabet, shift):
         letters_and_equivalents[alphabet_letters[i]] = alphabet_letters[i+shift]
 
     for i in range((len(alphabet_letters)-shift), len(alphabet_letters)):
+        letters_and_equivalents[alphabet_letters[i]] = alphabet_letters[(i+2) % 26]
+
+    """for i in range((len(alphabet_letters)-shift), len(alphabet_letters)):
         letters_and_equivalents[alphabet_letters(len(alphabet_letters[i] = alphabet_letters[j]
-        j = j+ 1
+        j = j+ 1"""
 
     for each_char in crypted_text:
         if each_char in letters_and_equivalents:
@@ -27,27 +30,26 @@ def decryption(crypted_text, the_alphabet, shift):
     return decrypted_string
 
 
-  letters_and_equivalents[alphabet_letters[i]] = alphabet_letters[(i+2) % 26]
-
-
 the_alphabet = "abcdefghijklmnopqrstuvwxyz"
 crypted_text = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 
-print decryption(crypted_text,the_alphabet)
+print decryption(crypted_text,the_alphabet,2)
 
 
 # version 2
 
-def decryption2(crypted_text):
+from string import maketrans
+def short_decryption(crypted_text):
 
-    intab = "abcdefghijklmnopqrstuvwxyz"
-    outtab = "cdefghijklmnopqrstuvwxyzab"
-    decrypted_text = maketrans(intab, outtab)
+    initial_letters = "abcdefghijklmnopqrstuvwxyz"
+    equivalent_letters = "cdefghijklmnopqrstuvwxyzab"
+    mapping = maketrans(initial_letters, equivalent_letters)
 
-    return decrypted_text
+    uncrypted_text = crypted_text.translate(mapping)
+
+    return uncrypted_text
 
 crypted_text = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
-
-print decryption()
+print short_decryption(crypted_text)
 
 
